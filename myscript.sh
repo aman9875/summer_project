0ff91b535f74152577fc2cd74ba7fd0bbdd67a4f
aman9875 <amanr@iitk.ac.in>
2017-06-17 10:22:43 -0400

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -3,7 +3,7 @@ s=0
 for((a=0;a<1000;a++))
 do
 	python setting_ships.py ships.txt
-	python battleship_final2.py ships.txt no_of_moves.txt
+	python battleship_test.py ships.txt no_of_moves.txt
 	SUM=0
 	for i in `cat no_of_moves.txt`
 	do
